65d6cabb46429c3f95475489b659741b10deb000
puddletowntom <ronandriscoll@outlook.com>
2023-08-03 16:25:42 +0100

    RTOS added

--- main/main.c
+++ main/main.c
@@ -15,72 +15,49 @@
 //#define ADS1230_HOST    SPI1_HOST
 #define ADS1230_HOST    HSPI_HOST
 char dispTxt[20];
+float weightVal = 0.0L;
 
 int32_t map(int32_t x) {
     return (int32_t) ((x*100)/3000); 
 }
 
-// void getADCValue(void *pvParameter){
-//     for(;;){
-//         float adc_val = 0.0L;
-//         static bool calibrateComplete = false;
-//         adc_val = ADS1230_read();
-//         if(adc_val < 50000 && calibrateComplete == false){
-//             calibrateWeight();
-//             calibrateComplete = true;
-//             printf("Calibration complete \n");
-//         }
-//         if(adc_val > 50000){
-//             adc_val = 0.0L;
-//         }
-//         float weightVal = getWeight(adc_val);
-//         sprintf(dispTxt, "%.3f", weightVal);
-//         ESP_LOGI("..", "ADC 1 running.");
-//         vTaskDelay(1000 / portTICK_PERIOD_MS);
-//     }
-// }
-
-extern void screen_init(void);
-
-// void lvgl_task(void* arg) {
-//     screen_init();
-//     ESP_LOGI("..", "LCD running.");
-//     ui_init();
-//     for (;;) {
-//         lv_task_handler();
-//         vTaskDelay(pdMS_TO_TICKS(10));
-//     }
-// }
-
-void app_main(void) {
-   // xTaskCreatePinnedToCore(lvgl_task, "LCD", 8 * 1024, NULL, 5, NULL, 1);
-   // xTaskCreatePinnedToCore(getADCValue, "ADC", 4 * 1024, NULL, 4, NULL, 1);
-   ADS1230_init();
-   screen_init();
-   ui_init();
-   while(1){
+void getADCValue(void *pvParameter){
+    for(;;){
+        ESP_LOGI("..", "ADC 1 running.");
         float adc_val = 0.0L;
         static bool calibrateComplete = false;
         adc_val = ADS1230_read();
         if(adc_val < 50000 && calibrateComplete == false){
             calibrateWeight();
             calibrateComplete = true;
-            printf("Calibration complete \n");
         }
         if(adc_val > 50000){
             adc_val = 0.0L;
         }
-        float weightVal = getWeight(adc_val);
+        weightVal = getWeight(adc_val);
         sprintf(dispTxt, "%.3f", weightVal);
+        vTaskDelay(pdMS_TO_TICKS(15));
+    }
+}
+
+extern void screen_init(void);
+
+void lvgl_task(void* arg) {
+    for (;;) {
         int numInt = map((int32_t)weightVal);
         lv_label_set_text(ui_Label2, dispTxt);//lv_label_set_text(label1, myStr);
         lv_meter_set_indicator_end_value(ui_meter, indic1, numInt);
         lv_timer_handler();
-        memset(dispTxt, 0, sizeof(dispTxt));
-        //ESP_LOGI("..", "Main running.");
-        //vTaskDelay(1000 / portTICK_PERIOD_MS);
         lv_task_handler();
+        memset(dispTxt, 0, sizeof(dispTxt));
         vTaskDelay(pdMS_TO_TICKS(10));
-       // vTaskDelay(1000 / portTICK_PERIOD_MS);
-   }
+    }
+}
+
+void app_main(void) {
+   ADS1230_init();
+   screen_init();
+   ui_init();
+   xTaskCreatePinnedToCore(lvgl_task, "LCD", 8 * 1024, NULL, 3, NULL, 1);
+   xTaskCreatePinnedToCore(getADCValue, "ADC", 4 * 1024, NULL, 5, NULL, 1);
 }
